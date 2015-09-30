25b3ff9baa7a266cd987c58db5088e1366a29b96
George Moreas <gmoreasjr@gmail.com>
2015-09-29 23:39:24 -0700

    Enhanced the script to display better output

--- gamblers_demise_demo.py
+++ gamblers_demise_demo.py
@@ -2,14 +2,12 @@ import math
 import numpy
 
 
-def r(capital=100, bet='even'):
+def r(capital=1000, bet='even'):
     init_capital = capital
-    bet_amount = 1
+    bet_amount = 10
     spin_count = 0
     max_profit = 0
 
-    print ' '
-    print 'You start with $100'
     print ' '
 
     while capital > 0:
@@ -23,7 +21,7 @@ def r(capital=100, bet='even'):
 
         if spin_result < 1:
             win = False
-        if spin_result % 2 == 0:
+        elif spin_result % 2 == 0:
             if bet == 'even':
                 win = True
             if bet == 'odd':
@@ -36,19 +34,27 @@ def r(capital=100, bet='even'):
 
         if win:
             capital += bet_amount
-            bet_amount = 1
+            result = '+ $' + str(bet_amount).ljust(6)
         else:
             capital -= bet_amount
-            bet_amount *= 2
+            result = '- $' + str(bet_amount).ljust(6)
 
         if capital - init_capital > max_profit:
             max_profit = capital - init_capital
 
-        print 'Spin ' + str(spin_count) + ": " + spin_result_str.ljust(2) + '     ' + \
-              'Bet amount: ' + str(bet_amount).ljust(4) + '     ' + \
+        print 'Spin ' + str(spin_count) + ": " + spin_result_str.ljust(6) + \
+              'Result: ' + result + \
               'Total Capital: $' + str(capital)
 
+        if win:
+            bet_amount = 10
+        else:
+            bet_amount *= 2
+
+    print ' '
+    print 'You started with $1000 and you bet $10 on even, doubling down if you lost.'
     print ' '
     print 'You lost in ' + str(spin_count) + ' moves. Your maximum profit was $' + \
           str(max_profit) + '.'
+    print ' '
 
